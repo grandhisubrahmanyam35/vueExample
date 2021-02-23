8d7f796369291764c052d150907fa8ef49982011
grandhisubrahmanyam35 <grandhi.subrahmanyam@applaudsolutions.com>
2021-02-23 15:17:54 +0530

    cmmited vue plugin task

---
+++ public/precache-manifest.83622a1f50c75509327336e666bb7c8b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1e4f5abe73513cde09",
+    "url": "css/app.601d2ada.css"
+  },
+  {
+    "revision": "7820967415a770d58a67",
+    "url": "css/chunk-vendors.e0eccded.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "712a13c5a2abfc35b8ab60b86074b1d0",
+    "url": "index.html"
+  },
+  {
+    "revision": "3b1e4f5abe73513cde09",
+    "url": "js/app.565b6328.js"
+  },
+  {
+    "revision": "7820967415a770d58a67",
+    "url": "js/chunk-vendors.3e1b21d4.js"
+  },
+  {
+    "revision": "6b2ac24c7e68745f6ff903cb7c82c92c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
